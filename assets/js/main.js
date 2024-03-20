18b8cfa5b2fb17257032c7ad69f17b610e4e0db6
Patrick Herbst <p.herbst.1987@gmail.com>
2024-03-20 16:09:19 +0100

    fixed a missing nav-link

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,23 +72,20 @@ const scrollUp = () => {
 window.addEventListener("scroll", scrollUp);
 
 // SCROLL SECTIONS ACTIVE LINK
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]');
 
 const scrollActive = () => {
-    // const scrollY = window.scrollY;
-
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 58;
-        const sectionId = current.getAttribute("id");
-        console.log(sectionId);
-        const sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add("active-link");
+            sectionsClass.classList.add('active-link');
         } else {
-            sectionsClass.classList.remove("active-link");
+            sectionsClass.classList.remove('active-link');
         }
     });
 }
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
