83a419b4a127c771d364d8d2327ebcef8beb8169
Patrick Herbst <p.herbst.1987@gmail.com>
2024-03-20 15:25:15 +0100

    section links in the nav bar are now active when scrolling through them

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,34 @@ const sendEmail = (e) => {
 }
 
 contactForm.addEventListener("submit", sendEmail)
+
+// SHOW SCROLL UP
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+
+    // When the scroll is higher than 250 viewport height, add the show-scroll class to the a tag with the scrollup
+    this.scrollY >= 250 ? scrollUp.classList.add("show-scroll")
+                        : scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+        const sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+}
+window.addEventListener("scroll", scrollActive);
